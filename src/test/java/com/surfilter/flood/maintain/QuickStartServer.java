bf54ff490dbda434248960f6e62fed26b4cbcf0f
kefei wang <327724926@qq.com>
2015-02-02 14:55:16 +0800

    push jetty start

--- src/test/java/com/surfilter/flood/maintain/QuickStartServer.java
+++ src/test/java/com/surfilter/flood/maintain/QuickStartServer.java
@@ -17,7 +17,7 @@ import org.springside.modules.test.spring.Profiles;
 public class QuickStartServer {
 
 	public static final int PORT = 8080;
-	public static final String CONTEXT = "/quickstart";
+	public static final String CONTEXT = "/maintain";
 	public static final String[] TLD_JAR_NAMES = new String[] { "sitemesh", "spring-webmvc", "shiro-web",
 			"springside-core" };
 
